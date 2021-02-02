303553584b129b9eaaac5d3e64b7fdcdc287365c
Marekkon5 <marekkon5@yahoo.com>
2021-02-02 21:57:49 +0100

    Remove padding

--- src/storage.rs
+++ src/storage.rs
@@ -88,7 +88,7 @@ where
 {
     /// Creates a new storage with a default padding of 2048 bytes and no shrinkage.
     pub fn new(file: F, region: ops::Range<u64>) -> PlainStorage<F> {
-        PlainStorage::with_padding(file, region, 2048, None)
+        PlainStorage::with_padding(file, region, 0, Some(0))
     }
 
     /// Creates a new storage with the specified amount of padding.
